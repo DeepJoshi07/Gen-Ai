d8108826be748e7da36e9a673041e7decd55ae04
DeepJoshi07 <joshideep972@gmail.com>
2026-07-02 07:50:32 +0530

    tuples

---
+++ 02_datatypes/05_tuples.py
@@ -0,0 +1,13 @@
+masala_spices = ("cardamom","cloves","cinnamom")
+(s1,s2,s3) = masala_spices
+print(f"main masala spices: {s1}, {s2}, {s3}.")
+
+ratio1,ratio2 = 2,1
+print(f"Ratio is G :{ratio1} and C: {ratio2}")
+ratio1,ratio2 = ratio2,ratio1
+print(f"Ratio is G :{ratio1} and C: {ratio2}")
+
+# membership
+
+print(f"is ginger in masala spices ? {"ginger" in masala_spices}")
+print(f"is cinnamom in masala spices ? {"cinnamom" in masala_spices}")
