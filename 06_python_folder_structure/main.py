1a8b9a956bc76728029f40f9ecdbe0ce90d4b10e
DeepJoshi07 <joshideep972@gmail.com>
2026-07-06 14:30:00 +0530

    imports in python

---
+++ 06_python_folder_structure/main.py
@@ -0,0 +1,9 @@
+import recipes.flavors
+
+print(recipes.flavors.elichi_chai())
+
+# or
+
+from recipes.flavors import elichi_chai
+
+print(elichi_chai())
---
+++ 06_python_folder_structure/recipes/flavors.py
@@ -0,0 +1,5 @@
+def elichi_chai():
+    return "elichi chai is ready."
+
+def ginger_chai():
+    return "ginger chai is ready."
