3905bd7dac167137b34ee764084e6ca12e8cfc70
DeepJoshi07 <joshideep972@gmail.com>
2026-07-07 15:19:32 +0530

    close in generator

---
+++ 08_generator_and_decorator/generator_closing.py
@@ -0,0 +1,31 @@
+def indian_chai():
+    yield "masala chai"
+    yield "elichi chai"
+    yield "ginger chai"
+
+def foreign_chai():
+    yield "lemon tea"
+    yield "macha"
+    yield "Oolong"
+
+def all_chais_menu():
+    yield from indian_chai()
+    yield from foreign_chai()
+
+chais = all_chais_menu()
+
+for chai in chais:
+    print(chai)
+
+def chai_stall():
+    try:
+        while True:
+            order = yield "waiting for chai order."
+    except:
+        print("there was no chai order!")
+
+stall = chai_stall()
+
+print(next(stall))
+
+stall.close()
