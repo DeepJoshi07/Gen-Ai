0ee1b30a3a72baec4cd840821925c74dbe9b1e09
DeepJoshi07 <joshideep972@gmail.com>
2026-07-08 09:03:51 +0530

    methods in python class

---
+++ 09_oops/02_methods.py
@@ -0,0 +1,27 @@
+class Chai:
+    origin = "india"
+
+    def describe(self):
+        return(f"this chai originated in {self.origin}")
+    
+
+masala = Chai()
+print(masala.describe())
+print(Chai.describe(masala))
+masala.origin = "Nepal" 
+print(masala.origin)
+print(Chai.origin)
+
+print("----------------------------------------------------")
+# constroctor
+
+class ChaiOrder:
+    def __init__(self,type_,price):
+        self.type = type_
+        self.price = price
+
+    def self_print(self):
+        return(f"the chai you ordered is {self.type} and price is {self.price}")
+
+lemon = ChaiOrder("lemon",50)
+print(lemon.self_print())
