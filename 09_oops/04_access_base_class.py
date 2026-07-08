3823a12ff6277925f3d826b42f6a71261249b945
DeepJoshi07 <joshideep972@gmail.com>
2026-07-08 12:50:22 +0530

    MRO => method resolution order

--- 09_oops/04_access_base_class.py
+++ 09_oops/04_access_base_class.py
@@ -19,3 +19,20 @@ class Ginger(Chai):
     def __init__(self, type_, milk, suger):
         super().__init__(type_, milk)
         self.suger = suger
+
+# method resolution order
+
+class A:
+    label = "class A label"
+
+class B(A):
+    label = "class B label"
+
+class C(A):
+    label = "class C label"
+
+class D(B,C):
+    pass
+
+label = D()
+print(D.label)
