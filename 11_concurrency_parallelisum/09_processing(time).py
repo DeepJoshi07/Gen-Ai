fd1a994e1c116eddb74b32ef1ba40c1c56a460b5
DeepJoshi07 <joshideep972@gmail.com>
2026-07-11 15:13:00 +0530

    parallelism[Time]

---
+++ 11_concurrency_parallelisum/09_processing(time).py
@@ -0,0 +1,22 @@
+from multiprocessing import Process
+import time
+
+def cpu_havy():
+    print("process has started.")
+    total = 0
+    for _ in range(10**8):
+        total+=1
+    print("Done.")
+
+if __name__ == "__main__":
+
+    processes = [Process(target=cpu_havy) for _ in range(2)]
+
+    start = time.time()
+
+    [p.start() for p in processes]
+    [p.join() for p in processes]
+
+    end = time.time()
+
+    print(f"total time taken is: {end-start:.2f}")
